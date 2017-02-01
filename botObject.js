346833fd03c2190d1f8a8cee5574e3abb77c1066
Rishabh Saxena <rishabh.saxena1994@gmail.com>
2017-02-01 12:37:06 +0530

    Creation of Bot Object

---
+++ botObject.js
@@ -0,0 +1,7 @@
+var motionBot = function bot(xCoordinate,yCoordinate,direction)
+{
+xCoordinate = this.xCoordinate;
+yCoordinate = this.yCoordinate;
+direction = this.direction;
+}
+module.exports = motionBot;
